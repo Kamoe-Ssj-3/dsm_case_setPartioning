e3703f953b8bc356e300c6f876d5ed79fe837e40
Kamoe Ssj 3 <kamleshjagesser@gmail.com>
2025-03-04 09:42:53 +0100

    1e versie

--- pricing_algorithm.py
+++ pricing_algorithm.py
@@ -1,56 +1,77 @@
-
 class PricingAlgorithm:
 
     def __init__(self, problem):
         self.problem = problem
+        self.columns = []
+
 
     def find(self, lambdas, mu, sigma):
+        self.columns = []
+
         sortedShipments = self.sortShipments(lambdas)
+        sortedShipments = sortedShipments[94:]
 
         for lambda_value, shipment in sortedShipments:
             country = shipment.country
             postalCode = shipment.postalCode
             startPoint = shipment.startingPoint
             weight = shipment.weight
-            dangerous = shipment.is_dangerous
+            dangerous = shipment.isDangerous
             month = shipment.month
 
+            # if country == "null":
+            #     for warehouse in self.problem.warehouses:
+            #         j
+            # else:
             if startPoint == "CH01":
                 tildeCost = self.calculateTildeCost(country, postalCode, startPoint, weight, dangerous)
-                self.findColummns(tildeCost, lambda_value, shipment, mu[month], startPoint)
+                self.findDirectPair(tildeCost, lambda_value, shipment, mu[month], startPoint)
 
-                self.addWarehouseRedCost(shipment, country, postalCode, startPoint, weight, dangerous, month,
-                                         lambda_value, sigma)
+                self.findWarehousePair(shipment, country, postalCode, startPoint, weight, dangerous, month,
+                                       lambda_value, sigma)
             else:
-                self.addWarehouseRedCost(shipment, country, postalCode, startPoint, weight, dangerous, month,
-                                         lambda_value, sigma)
+                self.findWarehousePair(shipment, country, postalCode, startPoint, weight, dangerous, month,
+                                       lambda_value, sigma)
+        return self.columns
 
-    def addWarehouseRedCost(self, shipment, country, postalCode, startPoint, weight, dangerous, month, lambda_value,
-                            sigma):
+    def findWarehousePair(self, shipment, country, postalCode, startPoint, weight, dangerous, month, lambda_value,
+                          sigma):
         for warehouse in self.problem.warehouses:
             warehouseId = warehouse.warehouseId
+
+            if warehouseId == ("WH1" or "WH2"):
+                tildeCost = self.calculateTildeCost(warehouse.country, warehouse.postalCode, startPoint, weight,
+                                                    dangerous)
+                tildeCost += self.calculateTildeCost(country, postalCode, startPoint, weight, dangerous)
 
             tildeCost = self.calculateTildeCost(warehouse.country, warehouse.postalCode, startPoint, weight, dangerous)
             tildeCost += self.calculateTildeCost(country, postalCode, warehouseId, weight, dangerous)
 
-            self.findColummns(tildeCost, lambda_value, shipment, sigma[warehouseId, month], warehouseId)
+            self.findDirectPair(tildeCost, lambda_value, shipment, sigma[warehouseId, month], warehouseId)
 
-    def findColummns(self, tildeCost, lambda_value, shipment, dual, startPoint):
+    def findDirectPair(self, tildeCost, lambda_value, shipment, dual, startPoint):
         reducedCost = self.calculateRedCost(tildeCost, lambda_value, shipment, dual)
         if reducedCost < 0:
-            self.reducedCostList.append([reducedCost, startPoint, shipment])
+            self.columns.append([tildeCost, startPoint, shipment])
 
     def sortShipments(self, lambdas):
         sorted_lambdas = sorted(enumerate(lambdas), key=lambda x: x[1], reverse=True)
-        sorted_data = [(value, index, self.problem.shipments[index]) for index, value in sorted_lambdas]
+        print("lambdas, gesorteerde volgorde")
+        print(sorted_lambdas[:1000])
+        # In 2e iteratie argmax lamda opslaan, dan self.problem.shipments[argmax] printen, en kijken of pairing goed gaat.
+        print("shipments, originele volgorde")
+        print(self.problem.shipments[:1000])
+        sorted_data = [(value, self.problem.shipments[index]) for index, value in sorted_lambdas]
+        print("lambdas en shipments pairs, gesorteerde volgorde")
+        print(sorted_data[:1000])
         return sorted_data
 
     def calculateTildeCost(self, country, postalCode, startPoint, weight, dangerous):
-        route_cost = self.routeCostDict.calculateRouteCost(country, postalCode, startPoint, weight, dangerous)
+        route_cost = self.problem.routeCostDictionary.calculateRouteCost(country, postalCode, startPoint, weight, dangerous)
         kilometers = self.problem.routeCostDictionary.getDistance(country, postalCode, startPoint)
         leadtime = self.problem.routeCostDictionary.getLeadtime(country, postalCode, startPoint)
 
-        return route_cost + (alpha * eta * kilometers) + (beta * leadtime)
+        return route_cost + (self.problem.alpha * self.problem.eta * kilometers) + (self.problem.beta * leadtime)
 
     def calculateRedCost(self, tildeCost, lambdaValue, shipment, secondDual):
         return tildeCost - lambdaValue - shipment.weight * secondDual
--- route_cost.py
+++ route_cost.py
@@ -65,14 +65,66 @@ class RouteCost:
 
         return result
 
-    def getCost(self, country, postal_code, dep_point, weight):
+    def searchCHStartingPoint(self, country, postal_code, startPoint):
+        possible_primary = ["CH00", "CH01", "CH02"]
+        possible_secondary = ["CH04", "CHS1"]
+
+        # Check if the given dep_shipping_point has "Minimum"
+        try:
+            if "Minimum" in self.routeCostDictionary[country][postal_code][startPoint]:
+                return startPoint
+        except KeyError:
+            pass  # If key doesn't exist, continue checking other options
+
+        # Store the best option (lowest cost and corresponding dep_shipping_point)
+        min_cost = float("inf")
+        best_point = None
+
+        # Check among CH00, CH01, CH02
+        for alt_point in possible_primary:
+            try:
+                cost = self.routeCostDictionary[country][postal_code][alt_point]["Minimum"]
+                if cost < min_cost:
+                    min_cost = cost
+                    best_point = alt_point
+            except KeyError:
+                continue  # Skip if key doesn't exist
+
+        if best_point:
+            return best_point  # Return the best dep_shipping_point from the primary set
+
+        # If no valid point found, check CH04, CHS1
+        for alt_point in possible_secondary:
+            try:
+                cost = self.routeCostDictionary[country][postal_code][alt_point]["Minimum"]
+                if cost < min_cost:
+                    min_cost = cost
+                    best_point = alt_point
+            except KeyError:
+                continue  # Skip if key doesn't exist
+
+        return best_point  # Return the best dep_shipping_point found, or None if none exist
+
+    def getCost(self, country, postal_code, startPoint, weight):
         """
         Retrieves the cost for a given weight by finding the smallest weight category that is greater than or equal to the given weight.
         """
-        minCost = self.routeCostDictionary[country][postal_code][dep_point]["Minimum"]
+        cost = 0
+        CHList = ["CH00", "CH01", "CH02"]
+
+        if startPoint in CHList:
+            startPoint = self.searchCHStartingPoint(country, postal_code, startPoint)
+
+        if weight > 20000:
+            cost = self.routeCostDictionary[country][postal_code][startPoint]["FTL"]
+            weight -= 20000
+
+        minCost = self.routeCostDictionary[country][postal_code][startPoint]["Minimum"]
         for max_weight, cost_category in self.weight_categories:
             if weight <= max_weight:
-                cost = self.routeCostDictionary[country][postal_code][dep_point][cost_category]
+                customClearance = self.routeCostDictionary[country][postal_code][startPoint]["Custom Clearance"]
+                cost += self.routeCostDictionary[country][postal_code][startPoint][cost_category] / 100 * weight
+                cost += customClearance
                 if cost < minCost:
                     return minCost
                 else:
